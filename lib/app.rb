b8d103f293a9d8c753656e8d91c6a083ef55fa63
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-22 13:33:41 -0700

    Good fail initialize Number Class. Co-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>

--- lib/app.rb
+++ lib/app.rb
@@ -1,1 +1,15 @@
 #!/usr/bin/env ruby
+
+# {hundred, thousand, million, billion, trillion}
+# {one, two, three, ..., nineteen}
+# {twenty, thirty, forty, ..., ninety}
+
+class Words
+  def initialize(number)
+    @number = number
+  end
+
+  def number
+    @number
+  end
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,2 +1,9 @@
 require('rspec')
 require('app')
+
+describe('Number#get_number')  do
+  it('initializes Number class') do
+    test = Words.new(1)
+    expect(test.number).to(eq())
+  end
+end

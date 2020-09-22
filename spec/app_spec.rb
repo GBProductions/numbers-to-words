0886a40ed93dd394696901921c7b8b26ec83eb47
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-22 13:34:28 -0700

    Pass test, initialize Words class. Co-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,6 +4,6 @@ require('app')
 describe('Number#get_number')  do
   it('initializes Number class') do
     test = Words.new(1)
-    expect(test.number).to(eq())
+    expect(test.number).to(eq(1))
   end
 end

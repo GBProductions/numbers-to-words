27e9d911aa4facd62d711a40532b190b68fb6f3a
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-22 13:48:06 -0700

    Test singledigit definitions. Co-authored-by: Joseph Karnafel <josephkarnafel@gmail.com>

--- lib/app.rb
+++ lib/app.rb
@@ -7,9 +7,14 @@
 class Words
   def initialize(number)
     @number = number
+    @singledigit = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
   end
 
   def number
     @number
+  end
+
+  def singledigit
+    @singledigit
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,9 +1,13 @@
 require('rspec')
 require('app')
 
-describe('Number#get_number')  do
-  it('initializes Number class') do
+describe('Words#get_number')  do
+  it('initializes Words class') do
     test = Words.new(1)
     expect(test.number).to(eq(1))
+  end
+  it ('initialize singledigit definitions') do
+    test = Words.new('')
+    expect(test.singledigit).to(eq({1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}))
   end
 end
